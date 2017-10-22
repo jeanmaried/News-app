9409458a5b3a7e91f87ab4513f2cd5f0b322d696
Joseph Dalmasso <me@jodalmasso.com>
2017-10-22 00:00:38 -0400

    fixed dropdown

--- comp/js/index.js
+++ comp/js/index.js
@@ -1,43 +1,49 @@
 $("#dropdown").on("change", function(){
 	var selected = $("#dropdown option:selected").val();
+	if (selected === "sections"){
+		$(".newsBlock").remove();
+		$("header").removeClass("minified");
+	}else{
+		var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+		url += '?' + $.param({
+			'api-key': "811f3df9ce3c4d5bb2ba5ee608d21bd3"
+		});
 
-var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
-url += '?' + $.param({
-  'api-key': "811f3df9ce3c4d5bb2ba5ee608d21bd3"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
+		$.ajax({
+			url: url,
+			method: 'GET',
+		}).done(function(result) {
+			console.log(result);
+			$("header").addClass("minified");
 
-}).done(function(result) {
-	$("header").addClass("minified");
-	var count = 1;
+			var count = 0;
 
-  for (var i=0; i<result.results.length; i++){
+			let results = result.results;
+			$(".newsBlock").html("");
+			for (var i=0; i < results.length; i++){
+				if (count <= 12){
+					if (results[i].multimedia.length){
+						var pic = results[i].multimedia[4].url;
+						var abstract = results[i].abstract;
+						var url = results[i].url;
+						var newDiv = $("<div class='img_class'></div>");	
+						newDiv.html('<a href='+url+' target="_blank"><div class="abstract">'+abstract+'</div></a>');
 
-  	if (count <= 12){
-  	if (result.results[i].multimedia.length){
-  	  	var pic = result.results[i].multimedia[4].url;	
-  	  	var n = $("<div></div>");	
-   		n.html('<div class="abstract">'+result.results[i].abstract+'</div>');
-		n.addClass("img_class");
-		n.css('background-image',"url("+pic+")");
-		$(".newsBlock").append(n);
-		$(count++);
-		}
+						$("div.abstract").text(function(index, currentText) {
+							return currentText.substr(0, 100) + "...";
+						});
+
+						newDiv.css('background-image',"url("+pic+")");
+						$(".newsBlock").append(newDiv);
+						$(count++);
+					}
+				}
+				else {
+					break;
+				};
+			};
+		}).fail(function(err) {
+			throw err;
+		});
 	}
-
-	else {
-		break;
-	};
- 
- };
-
-
-
-
-}).fail(function(err) {
-  throw err;
-});
-
 });
--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,38 +1,51 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 $("#dropdown").on("change", function () {
-  var selected = $("#dropdown option:selected").val();
+	var selected = $("#dropdown option:selected").val();
+	if (selected === "sections") {
+		$(".newsBlock").remove();
+		$("header").removeClass("minified");
+	} else {
+		var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+		url += '?' + $.param({
+			'api-key': "811f3df9ce3c4d5bb2ba5ee608d21bd3"
+		});
 
-  var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
-  url += '?' + $.param({
-    'api-key': "811f3df9ce3c4d5bb2ba5ee608d21bd3"
-  });
-  $.ajax({
-    url: url,
-    method: 'GET'
+		$.ajax({
+			url: url,
+			method: 'GET'
+		}).done(function (result) {
+			console.log(result);
+			$("header").addClass("minified");
 
-  }).done(function (result) {
-    $("header").addClass("minified");
-    var count = 1;
+			var count = 0;
 
-    for (var i = 0; i < result.results.length; i++) {
+			let results = result.results;
+			$(".newsBlock").html("");
+			for (var i = 0; i < results.length; i++) {
+				if (count <= 12) {
+					if (results[i].multimedia.length) {
+						var pic = results[i].multimedia[4].url;
+						var abstract = results[i].abstract;
+						var url = results[i].url;
+						var newDiv = $("<div class='img_class'></div>");
+						newDiv.html('<a href=' + url + ' target="_blank"><div class="abstract">' + abstract + '</div></a>');
 
-      if (count <= 12) {
-        if (result.results[i].multimedia.length) {
-          var pic = result.results[i].multimedia[4].url;
-          var n = $("<div></div>");
-          n.html('<div class="abstract">' + result.results[i].abstract + '</div>');
-          n.addClass("img_class");
-          n.css('background-image', "url(" + pic + ")");
-          $(".newsBlock").append(n);
-          $(count++);
-        }
-      } else {
-        break;
-      };
-    };
-  }).fail(function (err) {
-    throw err;
-  });
+						$("div.abstract").text(function (index, currentText) {
+							return currentText.substr(0, 100) + "...";
+						});
+
+						newDiv.css('background-image', "url(" + pic + ")");
+						$(".newsBlock").append(newDiv);
+						$(count++);
+					}
+				} else {
+					break;
+				};
+			};
+		}).fail(function (err) {
+			throw err;
+		});
+	}
 });
 
 },{}]},{},[1]);
